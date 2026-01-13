44a4e64755ba32f8b4ef1445f93471071e431569
unknown <screentapinc@gmail.com>
2026-01-13 13:08:16 +0100

    mbuen

--- src/app/g/[slug]/page.tsx
+++ src/app/g/[slug]/page.tsx
@@ -43,7 +43,7 @@ export async function generateMetadata({ params }: GalleryViewPageProps): Promis
       is_active: true,
       created_at: '',
       updated_at: ''
-    } : undefined 
+    } as any: undefined 
   });
 }
 
